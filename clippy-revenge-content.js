4af9bdccb7485f6588fc0fa4e2360b1fce477c1b
peter-james-obrien <peter.o'brien@infotrack.com.au>
2018-03-06 11:21:46 +1100

    more keithy

--- clippy-revenge-content.js
+++ clippy-revenge-content.js
@@ -122,12 +122,12 @@ clippy.load('Clippy', function (agent) {
 
   var urlchangy = function () {
     var yescall = function () { window.location.href = 'http://heeeeeeeey.com/'; }
-    agent.speak("I can't help but notice you are on some stupid web page. Want to let me just take you to a cool page?<br /><br /><a href=\"#\" class=\"clippyyes\">YES</a>    <a href=\"#\" class=\"clippyno\">NO</a>", true, callsf(yescall));
+    agent.speak("Hi Keithy, I can't help but notice you are on some stupid web page. Want to let me just take you to a cool page?<br /><br /><a href=\"#\" class=\"clippyyes\">YES</a>    <a href=\"#\" class=\"clippyno\">NO</a>", true, callsf(yescall));
   };
 
   var insulty = function () {
     agent.play('GetAttention');
-    agent.speak("You are bad at web browsing, and you should feel bad.");
+    agent.speak("Keithy, you are bad at web browsing, and you should feel bad.");
   };
 
   var formfilly = function () {
@@ -138,7 +138,7 @@ clippy.load('Clippy', function (agent) {
       $('textarea').val('You can fill this part out with alot of text! Bet you didnt know that!');
     };
     agent.play('Wave');
-    agent.speak("Did you know there are some forms to fill out on this page? Want me to fill them out for you?<br /><br /><a href=\"#\" class=\"clippyyes\">YES</a>    <a href=\"#\" class=\"clippyno\">NO</a>", true, callsf(yescall));
+    agent.speak("Hi Keithy, did you know there are some forms to fill out on this page? Want me to fill them out for you?<br /><br /><a href=\"#\" class=\"clippyyes\">YES</a>    <a href=\"#\" class=\"clippyno\">NO</a>", true, callsf(yescall));
   };
 
   var scrolly = function () {
@@ -146,12 +146,12 @@ clippy.load('Clippy', function (agent) {
       var toScroll = $('body').scrollTop() + 500;
       $('body').scrollTop(toScroll);
     }
-    agent.speak("You seem to be taking your time. Want me to scroll down for you?<br /><br /><a href=\"#\" class=\"clippyyes\">YES</a>    <a href=\"#\" class=\"clippyno\">NO</a>", true, callsf(yescall));
+    agent.speak("Hi Keithy, you seem to be taking your time. Want me to scroll down for you?<br /><br /><a href=\"#\" class=\"clippyyes\">YES</a>    <a href=\"#\" class=\"clippyno\">NO</a>", true, callsf(yescall));
   };
 
   var techy = function () {
     agent.play('GetTechy');
-    agent.speak("Did you know, the term \"surfing the web\" came about when a popular surfer ran into a spider web while on his laptop?");
+    agent.speak("Hi Keithy, did you know, the term \"surfing the web\" came about when a popular surfer ran into a spider web while on his laptop?");
   };
 
   var clicky = function () {
@@ -159,11 +159,11 @@ clippy.load('Clippy', function (agent) {
     var yescall = function () {
       $('a')[Math.floor(Math.random() * $('a').length)].click()
     }
-    agent.speak("I see some links... want me to pick one and click on it for you?<br /><br /><a href=\"#\" class=\"clippyyes\">YES</a>    <a href=\"#\" class=\"clippyno\">NO</a>", true, callsf(yescall));
+    agent.speak("Hi Keithy, I see some links... want me to pick one and click on it for you?<br /><br /><a href=\"#\" class=\"clippyyes\">YES</a>    <a href=\"#\" class=\"clippyno\">NO</a>", true, callsf(yescall));
   };
 
   var facty1 = function () {
-    agent.speak("Web fact: Internet Explorer is the worst. No one likes it. No one likes developing for it.");
+    agent.speak("Web fact: Internet Explorer is the worst. No one likes it. No one likes developing for it, except Keithy.");
   };
 
   var pleasy = function () {
@@ -176,11 +176,11 @@ clippy.load('Clippy', function (agent) {
     var yescall = function () {
       $('a').css({ color: "red", background: "blue" });
     }
-    agent.speak("Hey did you know there are links on this page? Want me to highlight them for you?<br /><br /><a href=\"#\" class=\"clippyyes\">YES</a>    <a href=\"#\" class=\"clippyno\">NO</a>", true, callsf(yescall));
+    agent.speak("Hi Keithy, did you know there are links on this page? Want me to highlight them for you?<br /><br /><a href=\"#\" class=\"clippyyes\">YES</a>    <a href=\"#\" class=\"clippyno\">NO</a>", true, callsf(yescall));
   };
 
   var facty2 = function () {
-    agent.speak("Web fact: the internet was first created by Keith von Infotrackhausen. True story.");
+    agent.speak("Web fact: the internet was first created by Keithy von Infotrackhausen. True story.");
   };
 
   var caty = function () {
@@ -188,16 +188,16 @@ clippy.load('Clippy', function (agent) {
     var yescall = function () {
       $('img').attr('src', 'http://cdn77.sadanduseless.com/wp-content/uploads/2014/03/derp3.jpg')
     }
-    agent.speak("This page needs more cats, should I put more cats on the page?<br /><br /><a href=\"#\" class=\"clippyyes\">YES</a>    <a href=\"#\" class=\"clippyno\">NO</a>", true, callsf(yescall));
+    agent.speak("Hi Keithy, this page needs more cats, should I put more cats on the page?<br /><br /><a href=\"#\" class=\"clippyyes\">YES</a>    <a href=\"#\" class=\"clippyyes\">NO</a>", true, callsf(yescall));
   };
 
   var colourChangy = function () {
     var yescall = function () {
-      $('body').css({ color: "orange", background: "grey" })
+      $('body').css({ color: "pink", background: "grey" })
       $('div').css({ color: "yellow", background: "green" })
       $('a').css({ color: "blue", background: "white" })
     }
-    agent.speak("I have a better color scheme, want me to switch to it?<br /><br /><a href=\"#\" class=\"clippyyes\">YES</a>    <a href=\"#\" class=\"clippyyes\">NO</a>", true, callsf(yescall));
+    agent.speak("Hi Keithy, it looks like you’re trying to release the platform, would you like a better colour scheme?<br /><br /><a href=\"#\" class=\"clippyyes\">YES</a>    <a href=\"#\" class=\"clippyyes\">NO</a>", true, callsf(yescall));
   };
 
   var animate = function () {
@@ -223,8 +223,8 @@ clippy.load('Clippy', function (agent) {
     facty2,
     linky,
     // animate,
+    // clicky,
     colourChangy,
-    clicky,
     urlchangy,
     urlchangy,
     urlchangy,
